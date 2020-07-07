0a849721dd2dfff78063d879da272d9257b48342
doesnotcompute10 <doesnotcompute10@outlook.com>
2020-07-07 20:13:53 +0100

    The full code has now became fully pythonic and is now ready for a new test. ERRORS: The code wont be able to run in pycarm (or at least on my local machine) as the Tkinter has not been applied to the VENV

--- Code.TkinterApplication.1.py
+++ Code.TkinterApplication.1.py
@@ -4,31 +4,29 @@ logonwin = Tk()
 username = "github_online_repo"
 password = "pythonissohard10"
 
-username_inn = Entry(logonwin, width=50)
+username_inn = Entry(logonwin, width=50, )
 username_inn.grid(row=1, column=0)
 
-usernameAnn = Label(logonwin, text="Username:")
-usernameAnn.grid(row=0, column=0)
+username_ann = Label(logonwin, text="Username:")
+username_ann.grid(row=0, column=0)
 
-passwordInn = Entry(logonwin, width=50)
-passwordInn.grid(row=1, column=1)
+password_inn = Entry(logonwin, width=50)
+password_inn.grid(row=1, column=1)
 
-passwordAnn = Label(logonwin, text="Password:")
-passwordAnn.grid(row=0, column=1)
+password_ann = Label(logonwin, text="Password:")
+password_ann.grid(row=0, column=1)
 
 
 def checkCredentials():
     global score
     score = 0
-    username_in = username_inn.get()
-    passwordIn = password_inn.get()
 
-    if username_in == username:
+    if username_inn.get() == username:
         score = score + 1
     else:
         print(" ")
 
-    if password_in == password:
+    if password_inn.get() == password:
         score = score + 1
     else:
         print(" ")
@@ -36,10 +34,9 @@ def checkCredentials():
     if score == 2:
         print("Activated")
     else:
-        print("Fail, please try again later")
+        print("Fail, please retry")
 
 
 userCheck = Button(logonwin, text="Logon", command=checkCredentials)
 userCheck.grid(row=1, column=2)
 
-## ABOVE IS NOT TO BE EDITED ##
