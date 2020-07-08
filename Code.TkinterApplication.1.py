ea5a18cc01e9fbbac825bf7b49be55e324df3210
Travis (doesnotcompute10) <67922197+doesnotcompute10@users.noreply.github.com>
2020-07-08 10:51:08 +0100

    Update Code.TkinterApplication.1.py

--- Code.TkinterApplication.1.py
+++ Code.TkinterApplication.1.py
@@ -4,16 +4,16 @@ logonwin = Tk()
 username = "github_online_repo"
 password = "pythonissohard10"
 
-username_inn = Entry(logonwin, width=50, )
+username_inn = Entry(logonwin, width=50, font=5)
 username_inn.grid(row=1, column=0)
 
-username_ann = Label(logonwin, text="Username:")
+username_ann = Label(logonwin, text="Username:", font=5)
 username_ann.grid(row=0, column=0)
 
-password_inn = Entry(logonwin, width=50)
+password_inn = Entry(logonwin, width=50, show="*", font=5)
 password_inn.grid(row=1, column=1)
 
-password_ann = Label(logonwin, text="Password:")
+password_ann = Label(logonwin, text="Password:", font=5)
 password_ann.grid(row=0, column=1)
 
 
@@ -37,6 +37,6 @@ def checkCredentials():
         print("Fail, please retry")
 
 
-userCheck = Button(logonwin, text="Logon", command=checkCredentials)
+userCheck = Button(logonwin, text="Logon", command=checkCredentials, font=5)
 userCheck.grid(row=1, column=2)
 
